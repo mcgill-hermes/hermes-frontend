209fb2c2d1bcc8d75d05560b995724aa90600bd6
fork1028 <xinyue_chen@icloud.com>
2022-02-16 15:31:10 -0500

    add router in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import "antd/dist/antd.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Navbar />}></Route>
+          <Route exact path="/signup"></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
