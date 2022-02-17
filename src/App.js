5fd5e0239a56a00dcbf171b0b6693a666796d1b0
yutian <yutianfuu@gmail.com>
2022-02-17 12:43:27 -0500

    Solve conlfict

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import Profile from "./components/Profile/Profile.js";
 import "antd/dist/antd.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Footer from "./components/Footer/Footer.js"
 function App() {
   return (
