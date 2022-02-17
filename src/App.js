d30bae1638f1adca2a417de5564a56939ec88be3
niezijin <zijin.nie@mail.mcgill.ca>
2022-02-17 00:30:22 -0500

    add register page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import "antd/dist/antd.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path="/" element={<Navbar />}></Route>
-          <Route exact path="/signup"></Route>
+          <Route exact path="/signup" element={<Register />}></Route>
           <Route exact path="/login" element={<Login />}></Route>
         </Routes>
       </div>
---
+++ src/components/Register.js
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "../actions/auth";
+import Navbar from './Navbar';
+import {
+  Form,
+  Input,
+  Alert,
+  Button,
+  Layout,
+  Row
+} from 'antd';
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 30,
+    },
+    sm: {
+      span: 10,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 10,
+    },
+  },
+};
+
+
+
+const Register = () => {
+  const [form] = Form.useForm();
+  const [successful, setSuccessful] = useState(false);
+
+  const { message } = useSelector(state => state.message);
+  const dispatch = useDispatch();
+
+
+
+  const onFinish = (values) => {
+    setSuccessful(false);
+
+    dispatch(register(values.username, values.password))
+      .then(() => {
+        setSuccessful(true);
+      })
+      .catch(() => {
+        setSuccessful(false);
+      });
+  };
+
+
+  return (
+    <div>
+      <Navbar />
+      <Layout>
+        <Layout>
+          <Row
+            type="flex"
+            justify="center"
+            align="middle"
+            style={{ minHeight: "100vh" }}
+          >
+            <Form
+              {...formItemLayout}
+              form={form}
+              name="register"
+              onFinish={onFinish}
+              scrollToFirstError
+            >
+
+              <Form.Item
+                name="username"
+                label="Username"
+                rules={[
+                  {
+                    required: true,
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                label="Password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                  {
+                    max: 20,
+                    min: 4
+                  }
+                ]}
+                hasFeedback
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item
+                name="confirm"
+                label="Confirm Password"
+                dependencies={['password']}
+                hasFeedback
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please confirm your password!',
+                  },
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue('password') === value) {
+                        return Promise.resolve();
+                      }
+
+                      return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                    },
+                  }),
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item {...tailFormItemLayout}>
+                <Button type="primary" htmlType="submit">
+                  Register
+                </Button>
+                {message && successful &&
+                  (<Alert message={message} type="success" showIcon />)
+                }
+                {message && !successful &&
+                  (<Alert message={message} type="error" showIcon />)
+                }
+              </Form.Item>
+            </Form>
+          </Row>
+        </Layout>
+      </Layout>
+    </div>
+  );
+};
+
+export default Register;
