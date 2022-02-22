c2845a0d561f1bfab34a326de6b8bb9a257440c7
fork1028 <xinyue_chen@icloud.com>
2022-02-22 16:17:38 -0500

    add preference in homepage

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import { Layout } from "antd";
+import Preference from "../Preference/Preference";
+import News from "../News/News";
 const { Content } = Layout;
 
 class Homepage extends Component {
   render() {
+    const preference = localStorage.getItem("preference");
+
+    const renderPage = () => {
+      if (preference === false || preference === null) {
+        return <Preference />;
+      } else {
+        return <News />;
+      }
+    };
+
     return (
       <div>
         <Layout>
-          <Content style={{ minHeight: "100vh" }}></Content>
+          <Content style={{ minHeight: "100vh" }}>{renderPage()}</Content>
         </Layout>
       </div>
     );
