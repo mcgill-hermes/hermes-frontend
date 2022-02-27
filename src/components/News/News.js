1cb9ec82d922e43ffaf51082a1d6257a38fcb3cc
fork1028 <xinyue_chen@icloud.com>
2022-02-27 14:05:59 -0500

    revise style in news

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout, Row } from "antd";
+import { Layout, Row, Col } from "antd";
 import { Tabs, Card, Spin } from "antd";
 import { useState, useEffect } from "react";
 
@@ -8,7 +8,7 @@ const { TabPane } = Tabs;
 const { Content, Header, Footer } = Layout;
 
 export default function News() {
-  const dummy =  [{},{},{},{},{},{}];
+  const dummy = [{}, {}, {}, {}, {}, {}];
 
   const [categories, setCategories] = useState([]);
   const [news, setNews] = useState(dummy);
@@ -21,58 +21,53 @@ export default function News() {
     fetch("http://localhost:8080/category/types", requestOptions).then(
       (response) => {
         if (response.ok) {
-          response.text().then(
-            (data) => {
-              const tags = JSON.parse(data)
-              const tagsAndAll = ["All"].concat(tags);
-              setCategories(tagsAndAll);
-            }
-          )
+          response.text().then((data) => {
+            const tags = JSON.parse(data);
+            const tagsAndAll = ["All"].concat(tags);
+            setCategories(tagsAndAll);
+          });
         } else {
-          console.log(response)
+          console.log(response);
         }
       }
-    )
+    );
     // const userName = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).userName : "testuser"
     fetch(`http://localhost:8080/news/get-all`, requestOptions).then(
       (response) => {
         if (response.ok) {
-          response.text().then(
-            (data) => {
-              // const tags = JSON.parse(data).map(o => o["type"])
-              setNews(JSON.parse(data));
-              setLoading(false);
-              // console.log(JSON.parse(data))
-            }
-          )
+          response.text().then((data) => {
+            // const tags = JSON.parse(data).map(o => o["type"])
+            setNews(JSON.parse(data));
+            setLoading(false);
+            // console.log(JSON.parse(data))
+          });
         } else {
-          console.log(response)
+          console.log(response);
         }
       }
-    )
-  }, [])
+    );
+  }, []);
 
   const callback = (e) => {
     setLoading(true);
-    setNews(dummy)
+    setNews(dummy);
     const requestOptions = {
       method: "GET",
     };
-    fetch("http://localhost:8080/news/Category/?type=" +categories[e], requestOptions).then(
-      (response) => {
-        if (response.ok) {
-          response.text().then(
-            (data) => {
-              setNews(JSON.parse(data));
-              setLoading(false);
-            }
-          )
-        } else {
-          console.log(response)
-        }
+    fetch(
+      "http://localhost:8080/news/Category/?type=" + categories[e],
+      requestOptions
+    ).then((response) => {
+      if (response.ok) {
+        response.text().then((data) => {
+          setNews(JSON.parse(data));
+          setLoading(false);
+        });
+      } else {
+        console.log(response);
       }
-    )
-  }
+    });
+  };
 
   return (
     <div className="News">
@@ -82,9 +77,15 @@ export default function News() {
           <Row
             type="flex"
             justify="center"
-            style={{ minHeight: "100vh", marginTop: "5%", marginBottom: "5%" }}
+            align="middle"
+            style={{
+              minHeight: "100vh",
+              marginTop: "5%",
+              marginBottom: "5%",
+              flexWrap: "wrap",
+            }}
           >
-            <Tabs type="card" onChange={callback}>
+            <Tabs type="card" onChange={callback} style={{ width: 800 }}>
               {categories.map((category, index) => {
                 return (
                   <TabPane tab={category} key={index}>
@@ -96,14 +97,14 @@ export default function News() {
                           size="small"
                           title={element.title}
                           extra={<a href={element.url}>Read more</a>}
-                          style={{ width: 700, marginTop: "10px" }}
+                          style={{ width: 800, marginTop: "10px" }}
                         >
                           <p>{element.content}</p>
                         </Card>
                       );
                     })}
                   </TabPane>
-                )
+                );
               })}
             </Tabs>
           </Row>
